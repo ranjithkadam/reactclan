ea819a74164f54f4e41fa65e28a1a2345f72b432
ranjith <ranjithkumar.kadam@gmail.com>
2026-01-05 20:24:43 +0530

    Props examples Practiced

--- src/main.jsx
+++ src/main.jsx
@@ -6,12 +6,16 @@ import App from './App.jsx'
  import {HeaderAdvance, HeaderComponent} from './components/header/Header.jsx';
 import { Footer } from './components/footer/Footer.jsx';
 import ReactProps from './props/example1.jsx';
- 
+import {Example2} from './props/example2.jsx';
 
+ 
+const userName="Ranjith Kumar";
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App/>
-
+     <Example2 name={userName}/>
+     <Example2 name="Rk"/>
+     
     {/* <HeaderComponent />
      <HeaderAdvance/>
      <Footer/> */}
---
+++ src/props/example2.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Example2 = function({name}){
+    return(
+        <div>
+            <h2>Welcome {name}</h2>
+        </div>
+    )
+}
+
+export {Example2}
